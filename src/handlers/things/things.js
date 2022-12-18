8d6100efcc5aa2f7683270703fca468c17594ec9
Achref.Yak <achref.yakdhane@esprit.tn>
2022-12-18 10:59:48 +0100

    create stack

--- src/handlers/things/things.js
+++ src/handlers/things/things.js
@@ -1,29 +1,22 @@
 const AppError = require('../../utils/AppError');
 const AppSuccess = require('../../utils/AppSuccess');
 
-const { Aws } = require('aws-cdk-lib');
-const IoT =require('aws-cdk-lib/aws-iot');
-const IAM = require('aws-cdk-lib/aws-iam');
-const ApI = require('aws-cdk-lib/aws-apigateway');
-const cdk = require('aws-cdk-lib');
+
 const ThingService = require('../../repository/thingService')
-const { App, Stack } = require('aws-cdk-lib');
-let thingTypeName = 'Type';
-let thingName = 'Type';
-let iotTopic = 'topic1';
+
+
 
  
 exports.create = async (event) => {
 
 
-    const app = new App();
- 
-    await  new cdk.Stack (app, "ThingService");
+    const thing = new ThingService().createStack();
 
-  app.synth();
-    
-
-  console.log(app);
+    if(thing)
+    {
+      return new AppSuccess(thing, 200).handleSuccess();
+    }
+    else return new AppError()
 
 
 
--- src/repository/thingService.js
+++ src/repository/thingService.js
@@ -1,30 +1,230 @@
 const dynamoose = require("dynamoose");
+const UserSchema = require('../schemas/userSchema');
 const bcrypt = require('bcryptjs');
+const logger = require('../utils/logger')
 const { v1: uuidv1 } = require('uuid');
-const { Aws } = require('aws-cdk-lib');
-const IoT =require('aws-cdk-lib/aws-iot');
-const IAM = require('aws-cdk-lib/aws-iam');
-const ApI = require('aws-cdk-lib/aws-apigateway');
-let thingTypeName = 'Type';
-let thingName = 'Type';
+const Aws = require('aws-sdk');
+const Iot = new Aws.Iot();
+const Iam = new Aws.IAM();
+
+let thingName = 'Type1';
 let iotTopic = 'topic1';
 const ThingSchema = require('../schemas/thingSchema');
-const  cdk = require('aws-cdk-lib');
 
-const { Construct } = require('constructs');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 
 
- class ThingService  extends Construct{
+
+
+class ThingService {
+  tableName = 'usersTable';
+  thingName = 'testThing';
+  thingTopic = 'testTopic';
+  dbInstance;
+  constructor() {
+
+    this.dbInstance = dynamoose.model(this.tableName, UserSchema);
+  }
+
+  createGroup() {
+    const params = {
+      thingGroupName: "testGroup",
+
+    }
+    return Iot.createThingGroup( params ).promise()
+
+
+
+
+  }
+
+  createPolicy(group) {
+
 
  
-    constructor(scope, id, props) {
-      super(scope, id);
+
+
+    let myManagedPolicy = {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": ["iot:Subscribe", "iot:Publish"],
+          "Resource": `arn:aws:iot:${process.env.REGION}:${process.env.ACCOUNT}:topic/iot/${this.thingTopic}`
+        },
+        {
+          "Effect": "Allow",
+          "Action": [
+            "iot:Connect",
+          ],
+          "Resource": `arn:aws:iot:${process.env.REGION}:${process.env.ACCOUNT}`+":client/${iot:Connection.Thing.ThingName}"
+        },
+
+      ]
+    };
+
+
+
+
+    var policyParams = {
+      policyDocument: JSON.stringify(myManagedPolicy),
+      policyName: 'iotGroupPolicy7',
+    };
+
+   return Iot.createPolicy(policyParams).promise().then( data => {
+      console.log("policy created");
+      return Iot.attachPolicy({policyName: policyParams.policyName, target: group.thingGroupArn}).promise().then( data => {
+        console.log("policy attached to group");
+        return policyParams.policyName;
+      }).catch(err => {
+        logger.error(err); 
+      })
+  
+   }).catch(err => {
+    logger.error(err);
+  })
+
+  }
+
+
+
+
+  createThingsInGroup(group, policyName){
+    console.log(policyName);
+    const name = "testThing-"+ uuidv1();
+    const params = {
+      thingName: name
+    }
+    return Iot.createThing(params).promise().then( thing => {
+      this.createThingCertificate(name, policyName).then(data => {
+        console.log("attached cert to thing");
+        return Iot.addThingToThingGroup({thingArn: thing.thingArn, thingGroupArn: group.thingGroupArn}).promise().then( data => {
+          console.log("attached thing to group");
+          
+        }).catch(err => {
+          logger.error(err); 
+        }
+        )
+      }).catch(err => console.log(err))
+  
+    })
+  }
+
+
+  createThingCertificate(name, _policyName) {
+    return Iot.createKeysAndCertificate({setAsActive: true}).promise().then(cert => {
+      console.log("cert created");
+      console.log(cert.certificatePem);
+      console.log(cert.keyPair.PrivateKey);
+      return Iot.attachThingPrincipal({thingName: name, principal: cert.certificateArn}).promise().then( data => {
+        console.log("thing attached to cert");
+       
+
+
+        }).catch(err => {
+          logger.error(err); 
+        })
       
+    }).catch(err => logger.error(err))
+  }
+
+
  
+
+  createStack(){
+
+  
+    return this.createGroup().then(
+
+      group => {
+
+
+        this.createPolicy(group).then( data => {
+          console.log("policy created");
+          for (let index = 0; index < 3; index++) {
+            this.createThingsInGroup(group, data).then( data => {
+        
+                console.log("attached thing to group");
+            }).catch(err => {
+              logger.error(err); 
+            });
+            
+          }
+        }).catch(err => {
+          logger.error(err);
+        })
+      
     
-  
-  }}
+       
+    
 
-  module.exports =  ThingService
+        })
 
-  
+
+
+ 
+ 
+
+
+  }
+
+
+  createRule() {
+    return Iot.createTopicRule({
+      ruleName: 'testRule',
+      topicRulePayload: {
+        actions: [
+          {
+            dynamoDB: {
+              hashKeyField: 'id',
+              hashKeyValue: '123',
+              hashKeyType: 'STRING',
+              payloadField: 'payload',
+              rangeKeyField: 'range',
+              rangeKeyValue: '456',
+              rangeKeyType: 'STRING',
+              tableName: 'testTable',
+              roleArn: 'arn:aws:iam::766779933142:role/iotserverless-dev-IotRole-N2ABXEYT02YN'
+            }
+          },
+        ],
+        awsIotSqlVersion: '2016-03-23',
+        description: 'test',
+        ruleDisabled: false,
+        sql: `SELECT * FROM 'iot/${this.thingTopic}'`
+
+      }
+    }).promise();
+  }
+
+  createSQSQueue() {
+
+    // create iot rule to send to sqs
+    return Iot.createTopicRule({
+      ruleName: 'testRule1',
+      topicRulePayload: {
+        actions: [
+          {
+            sqs: {
+              queueUrl: 'https://sqs.us-east-1.amazonaws.com/766779933142/testQueue',
+              roleArn: 'arn:aws:iam::766779933142:role/iotserverless-dev-IotRole-N2ABXEYT02YN',
+              useBase64: true
+            }
+          },
+        ],
+        awsIotSqlVersion: '2016-03-23',
+        description: 'test',
+        ruleDisabled: false,
+        sql: `SELECT * FROM 'iot/${this.thingTopic}'`
+
+      }
+    }).promise();
+
+  }
+
+
+}
+
+module.exports = ThingService
+
